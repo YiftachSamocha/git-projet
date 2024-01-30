2112b5a1d33f07549cf3876a0e8371ed6bfc53b9
Yiftach Samocha <yiftachsamo@gmail.com>
2024-01-31 01:13:32 +0200

    Limit ball size to 400px

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 function onBallClick() {
     var ball = document.querySelector('.ball');
+    if (ball.innerHTML === '400') {
+        ball.style.width = '100px'
+        ball.style.height = '100px'
+        ball.style.borderRadius = '100px'
+        ball.innerHTML = '100'
+        return
+    }
     var value = parseInt(ball.style.height) || 100;
     value += 50;
 
-    ball.style.width = value + 'px';
-    ball.style.height = value + 'px';
-    ball.style.borderRadius = value + 'px';
+    ball.style.width = value + 'px'
+    ball.style.height = value + 'px'
+    ball.style.borderRadius = value + 'px'
     ball.innerHTML = value
+
 
 }
